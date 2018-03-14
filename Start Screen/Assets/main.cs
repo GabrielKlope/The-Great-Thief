0642f927d5532a073ed4d570fe14f5c0cc8fb969
maryaliche <36933017+maryaliche@users.noreply.github.com>
2018-03-14 12:50:28 -0600

    Start Screen

--- Start Screen/Assets/main.cs
+++ Start Screen/Assets/main.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class main : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void QuitGame ()
+	{
+		Debug.Log ("Quit");
+		Application.Quit ();
 	}
 }
