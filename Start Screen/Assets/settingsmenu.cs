5e24f469acf1c63642dea090f001cc67f5096123
maryaliche <36933017+maryaliche@users.noreply.github.com>
2018-03-23 13:00:05 -0600

    Start Screen

--- Start Screen/Assets/settingsmenu.cs
+++ Start Screen/Assets/settingsmenu.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class settingsmenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public AudioMixer audioMixer;
+
+	public void SetVolume (float volume)
+	{
+		AudioMixer.SetFloat("Volume", Volume);
 	}
 }
