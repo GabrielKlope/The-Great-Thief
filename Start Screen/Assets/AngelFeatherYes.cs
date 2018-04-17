3892430c5f399e218afa6d2d97add56e0c0620fd
maryaliche <36933017+maryaliche@users.noreply.github.com>
2018-04-17 11:58:53 -0600

    Fixed simple error

--- Start Screen/Assets/AngelFeatherYes.cs
+++ Start Screen/Assets/AngelFeatherYes.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement
+using UnityEngine.SceneManagement;
 
 public class AngelFeatherYes : MonoBehaviour {
 
-	public viod PlayGame ()
+	public void PlayGame ()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1):
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 	}
 
 }
