3030c683547477b1eca6a03993f5b37aea67e6d6
Matthew Fitzgerald <matthewfitzgerald@ad.weber.edu>
2018-04-19 12:59:06 -0600

    DeathMenu

--- Start Screen/Assets/DeathMenu.cs
+++ Start Screen/Assets/DeathMenu.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DeathMenu : MonoBehaviour {
+
+	public void MainMenu ()
+	{
+		SceneManager.LoadScene ("Start Screen");
+	}
+
 
 	public void QuitButton ()
 	{
